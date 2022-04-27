2f9e59824cee6f121d6187a6414a07d2a919624e
akashN39 <103947839+akashN39@users.noreply.github.com>
2022-04-27 10:40:30 +0530

    Update assmt5.js

--- assmt5.js
+++ assmt5.js
@@ -1,9 +1,51 @@
-  const date = new Date();
+const date = new Date();
+
 const renderCalendar = () => {
   date.setDate(1);
 
+  const listEvents = {
+    "January": {
+      "16": ["Birthday", "Seema Sinha"],
+      "25": ["Anniversary", "Seema Sinha"]
+    },
+    "February": {
+      "22": ["Birthday", "Prateek Sinha"]
+    },
+    "March": {
+      "14": ["Birthday", "Ajay Kumar Sinha"]
+    },
+    "April": {
+      "30": ["Birthday", "Sweta Kumari"]
+    },
+    "May": {
+      "17": ["Birthday", "Pradeep Kumar"]
+    },
+    "June": {
+      "10": ["Birthday", "Abha Sinha"],
+      "27": ["Anniversary", "Jyoti Sinha"]
+    },
+    "August": {
+      "10": ["Birthday", "Sachin Sinha"]
+    },
+    "September": {
+      "26": ["Birthday", "Runnu Sinha"]
+    },
+    "October": {
+      "2": ["Birthday", "Pranay Sinha"]
+    },
+    "November": {
+      "4": ["Birthday", "Prajjwal Sinha"],
+      "22": ["Anniversary", "Abha Sinha"],
+      "23": ["Birthday", "Lalita Sinha"]
+    },
+    "December": {
+      "10": ["Birthday", "Jyoti Sinha"],
+      "1": ["Birthday", "Pranav Sinha"],
+      "25": ["Birthday", "Bhavya Sinha"],
+      "26": ["Birthday", "Pravin Kumar"]
+    }
+  };
   const monthDays = document.querySelector(".days");
-
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -17,7 +59,6 @@ const renderCalendar = () => {
   ).getDate();
 
   const firstDayIndex = date.getDay();
-
   const lastDayIndex = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -25,7 +66,6 @@ const renderCalendar = () => {
   ).getDay();
 
   const nextDays = 7 - lastDayIndex - 1;
-
   const months = [
     "January",
     "February",
@@ -38,26 +78,38 @@ const renderCalendar = () => {
     "September",
     "October",
     "November",
-    "December",
+    "December", 
   ];
+
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p"). innerHTML = new Date().toDateString();
 
-  document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-let days = "";
-
+  let days = "";
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    days += `<div class="prev-date">${prevLastDay - x +1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++) {
-    if (
+    if (listEvents.hasOwnProperty(months[date.getMonth()]) && listEvents[months[date.getMonth()]].hasOwnProperty(i)) {
+      var thisMonthEvents = listEvents[months[date.getMonth()]];
+      if (thisMonthEvents[i][0] === "Birthday" && i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today"><i class="fas fa-birthday-cake" style="color: #CBAB1A"></i></div>`
+      } else if (thisMonthEvents[i][0] === "Anniversary" && i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today"><i class="fas fa-heart" style="color:red;"></i></div>`
+      } else if (thisMonthEvents[i][0] === "Birthday") {
+        days += `<div class="normal-dates"><i class="fas fa-birthday-cake" style="color: #CBAB1A"></i></div>`
+      } else if (thisMonthEvents[i][0] === "Anniversary") {
+        days += `<div class="normal-dates"><i class="fas fa-heart" style="color:red;"></i></div>`
+      }
+    } else if (
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
       days += `<div class="today">${i}</div>`;
     } else {
-      days += `<div>${i}</div>`;
+      days += `<div class="normal-dates">${i}</div>`;
     }
   }
 
@@ -65,6 +117,21 @@ let days = "";
     days += `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
   }
+
+  document.querySelectorAll(".normal-dates").forEach(item => {
+    item.addEventListener("click", event => {
+      var targetDate = new Date();
+      targetDate.setDate(event.target.textContent);
+      targetDate.setMonth(date.getMonth());
+      if (listEvents.hasOwnProperty(months[targetDate.getMonth()])) {
+        var eventsThisMonth = listEvents[(months[targetDate.getMonth()])];
+        if (eventsThisMonth.hasOwnProperty(targetDate.getDate())) {
+          console.log(eventsThisMonth[targetDate.getDate()]);
+        }
+      }
+      console.log(targetDate.toDateString());
+    })
+  });
 };
 
 document.querySelector(".prev").addEventListener("click", () => {
@@ -76,5 +143,4 @@ document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
-
 renderCalendar();
